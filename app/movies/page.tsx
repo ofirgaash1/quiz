8433a3f3310bee1de4ecc3cb4ef475ba12f3d7bd
ofirgaash1 <ofir.gaash@gmail.com>
2025-02-16 10:39:41 +0200

    Update page.tsx

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,9 +1,9 @@
 import Uploader from '@/components/uploader'
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from '@/components/toaster'
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
+    <main className="relative flex flex-col items-center justify-center">
       <Toaster />
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
         Upload SRT file to the BLOB
